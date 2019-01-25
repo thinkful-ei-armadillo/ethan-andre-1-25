8446e2a030d4136903426fff3ab875adeb652ca3
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-01-25 12:09:30 -0500

    Add fetch functionality, and display images in DOM

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 'use strict';
 
+const state = {
+  dogImageUris: [],
+};
+
+const render = function () {
+
+  let html = '';
+
+  state.dogImageUris.forEach((uri) => {
+    html += `<li><img src="${uri}"></li>`;
+  });
+
+  $('#results').html(html);
+
+  console.log(state);
+};
+
 const main = function () {
 
   $('#dogs').submit((e) => {
@@ -8,10 +25,19 @@ const main = function () {
 
     const num = $('#num-dogs').val();
 
-    console.log(num);
+    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        state.dogImageUris = json.message;
+        render();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
-  console.log('boot');
 };
 
 $(main);
