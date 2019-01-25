caea915081dcedbe0bbb466caca1b339eb0446eb
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-01-25 14:43:31 -0500

    Update starting template

--- nps/index.js
+++ nps/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const main = function () {
+
+  $('#search').submit((e) => {
+
+    e.preventDefault();
+
+    const state = $('#state').val();
+    const maxResults = $('#max-results').val();
+
+    console.log(state, maxResults);
+  });
+};
+
+$(main);
